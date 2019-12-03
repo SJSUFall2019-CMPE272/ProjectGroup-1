fb9fdb1a4842384eb1b4ef4c0361368224c8feda
cvamsik <55032250+cvamsik@users.noreply.github.com>
2019-12-03 12:16:15 -0800

    Updated app.py

--- ML Model/app.py
+++ ML Model/app.py
@@ -3,69 +3,6 @@ import os
  
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from keras import ImageDataGenerator, load_img, img_to_array
-from keras.models import Sequential, load_model
-from flask_cors import CORS
-
-
-import numpy as np
-import argparse
-import imutils
-import cv2
-import time
-import uuid
-import base64
-
-img_width, img_height = 224, 224
-model_path = './models/model.h5'
-model_weights_path = './models/weights.h5'
-model = load_model(model_path)
-#model.load_weights(model_weights_path)
-
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
-
-def get_as_base64(url):
-    return base64.b64encode(request.get(url).content)
-
-def predict(file):
-    x = load_img(file, target_size=(img_width,img_height))
-    x = img_to_array(x)
-    x = np.expand_dims(x, axis=0)
-    array = model.predict(x)
-    result = array[0]
-    answer = np.argmax(result)
-    if answer == 0:
-        print("Label: Daisy")
-    elif answer == 1:
-	    print("Label: Rose")
-    elif answer == 2:
-	    print("Label: Sunflower")
-    return answer
-
-def my_random_string(string_length=10):
-    """Returns a random string of length string_length."""
-    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
-    random = random.upper() # Make all characters uppercase.
-    random = random.replace("-","") # Remove the UUID '-'.
-    return random[0:string_length] # Return the random string.
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-app = Flask(__name__)
-cors = CORS(app, resources={r"/": {"origins": "*"}})
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-@app.route("/")
-def template_test():
-    return render_template('template.html', label='', imagesource='../uploads/template.jpg')
-#Usage: python app.py
-import os
- 
-from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.models import Sequential, load_model
 from flask_cors import CORS
@@ -175,4 +112,4 @@ app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
 
 if __name__ == "__main__":
     app.debug=False
-    app.run(host='54.183.62.17', port=3200,threaded=False)
+    app.run((host='54.183.62.17', port=3200,threaded=False)
